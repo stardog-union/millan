402442038157ff97072b9464bf65cc1bf7c73247
Jason Rogers <jason@stardog.com>
2019-02-11 16:49:58 -0500

    Update types and built assets

--- dist/types/srs/SrsParser.d.ts
+++ dist/types/srs/SrsParser.d.ts
@@ -5,16 +5,18 @@ export declare class SrsParser extends TurtleParser {
     protected lexer: Lexer;
     protected namespacesMap: {};
     constructor(config?: Partial<IParserConfig>);
+    private visitCst;
+    private getSparqlRulesFromVisitor;
+    protected resetManagedState: () => void;
+    tokenize: (document: string) => IToken[];
+    parse: (document: string) => {
+        errors: IRecognitionException[];
+        semanticErrors: IRecognitionException[];
+        cst: any;
+    };
     SrsDoc: (idxInCallingRule?: number, ...args: any[]) => any;
     RuleDoc: (idxInCallingRule?: number, ...args: any[]) => any;
     RuleClause: (idxInCallingRule?: number, ...args: any[]) => any;
     IfClause: (idxInCallingRule?: number, ...args: any[]) => any;
     ThenClause: (idxInCallingRule?: number, ...args: any[]) => any;
-    protected resetManagedState: () => void;
-    tokenize: (document: string) => IToken[];
-    parse: (document: string) => {
-        errors: IRecognitionException[];
-        semanticErrors?: IRecognitionException[];
-        cst: any;
-    };
 }
