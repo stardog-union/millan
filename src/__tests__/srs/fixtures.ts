260eee6c45f157270a7f1251eeca6c333fe19617
Jason Rogers <jmrog@users.noreply.github.com>
2019-12-02 12:57:38 -0500

    Allow special Stardog functions in SRS (close #42) (#45)
    * Allow special Stardog functions in SRS (close #43)
    
    * Update fixtures and snapshots
    
    * Fix fixture again

--- src/__tests__/srs/fixtures.ts
+++ src/__tests__/srs/fixtures.ts
@@ -69,6 +69,23 @@ const ungeneratedFixtures = {
       'THEN {' +
       '   ?t :area ?area\n' +
       '}',
+    stardogOrCustomFunctions:
+      'PREFIX rule: <tag:stardog:api:rule:>\n\n' +
+      'RULE rules:LeaseTerminatedEarlyRule\n' +
+      'IF {\n' +
+      '    ?l a ro:ActiveLease .\n' +
+      '    ?lease_event ro:lease ?l ;\n' +
+      '        a ro:LeaseEvent ;\n' +
+      '        ops:EventDate ?event_dt ;\n' +
+      '       rdfs:label ?event_name .\n' +
+      '    BIND (IF(strstarts(?event_name, "Early Termination"), "true"^^xsd:boolean, "false"^^xsd:boolean) as ?terminated_early)\n' +
+      '    # Stardog-supported function:\n' +
+      '    BIND (date(?event_dt) as ?event_date)\n' +
+      '}\n' +
+      'THEN {\n' +
+      '    ?l ro:terminatedEarly ?terminated_early ;\n' +
+      '        ro:earlyTerminationDate ?event_date .\n' +
+      '}\n',
     // NOTE: These are valid only when the parser is run in 'stardog' mode.
     embedded_basic:
       '@prefix : <http://example.org/> .\n' +
